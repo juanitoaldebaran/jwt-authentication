a8a57d46eba930a085974fe2903b7ba522c25dfc
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-08-07 17:19:40 +0100

    add user repo

---
+++ backend/jwt-authentication/src/main/java/com/aldebaran/jwt_authentication/repository/UserRepo.java
@@ -0,0 +1,11 @@
+package com.aldebaran.jwt_authentication.repository;
+
+import com.aldebaran.jwt_authentication.model.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface UserRepo extends JpaRepository<UserModel, Integer> {
+    Optional<UserModel> findUserByEmail(String email);
+}
