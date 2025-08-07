6f8c2711345eafcfb12d6dfa317addb4e743315e
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-08-07 16:49:18 +0100

    add getter and setter with constructor in user model

--- backend/jwt-authentication/src/main/java/com/aldebaran/jwt_authentication/model/UserModel.java
+++ backend/jwt-authentication/src/main/java/com/aldebaran/jwt_authentication/model/UserModel.java
@@ -1,7 +1,6 @@
 package com.aldebaran.jwt_authentication.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.security.core.GrantedAuthority;
@@ -12,7 +11,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-@Data
 @Entity
 @Table (name = "user_table")
 public class UserModel implements UserDetails {
@@ -43,6 +41,84 @@ public class UserModel implements UserDetails {
     @Column (name = "updated_at")
     private Date updatedAt;
 
+    public UserModel(Integer id, String firstName, String lastName, String email, String password, MemberType memberType, Date issuedAt, Date updatedAt) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.memberType = memberType;
+        this.issuedAt = issuedAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public UserModel setId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public UserModel setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public UserModel setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public UserModel setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public UserModel setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public MemberType getMemberType() {
+        return memberType;
+    }
+
+    public UserModel setMemberType(MemberType memberType) {
+        this.memberType = memberType;
+        return this;
+    }
+
+    public Date getIssuedAt() {
+        return issuedAt;
+    }
+
+    public UserModel setIssuedAt(Date issuedAt) {
+        this.issuedAt = issuedAt;
+        return this;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public UserModel setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+        return this;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
