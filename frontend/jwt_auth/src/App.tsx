a1d85a9da5eac61707022ef10259ddccdf89fffb
juanitosiahaan1711@gmail.com <juanitosiahaan1711@gmail.com>
2025-08-08 22:53:17 +0100

    add folder setup

--- frontend/jwt_auth/src/App.tsx
+++ frontend/jwt_auth/src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <Router>
+      <Routes>
+        <Route>
+
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
